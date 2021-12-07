dd7a3743497f7049ff5635af879bff8fdc5ef521
krasa <sakke9907@gmail.com>
2021-12-07 16:44:24 +0400

    modal window

--- src/js/Main.js
+++ src/js/Main.js
@@ -20,8 +20,6 @@ export default class Main extends React.Component {
 
     closeModal = () => this.setState({setIsOpen: !this.state.setIsOpen});
 
-
-
     componentDidMount() {
         fetch("https://api.randomuser.me/1.0/?results=50&nat=gb,us&inc=gender,name,location,email,phone,picture")
             .then(res => res.json())
@@ -52,11 +50,19 @@ export default class Main extends React.Component {
                 <ul>
                     {items.map(item => (
                         <li key={item.name}>
-                            <img src={item.picture.medium} onClick={this.openModal} alt="Avatar"/>
+                            <img src={item.picture.medium} onClick={this.openModal} alt="picture"/>
                             <ModalWindow     show={this.state.setIsOpen} handleClose={this.closeModal}>
-                                <span>Something</span>
+                                <img src={item.picture.large} alt="picture"/>
+                                <ul>
+                                    <li>{item.location.stree}</li>
+                                    <li>{item.location.city}</li>
+                                    <li>{item.location.state}</li>
+                                    <li>{item.email}</li>
+                                    <li>{item.phone}</li>
+                                    <li>{item.location.postcode}</li>
+                                </ul>
                             </ModalWindow><br/>
-                            {item.name.title} {item.name.first} {item.name.last} <br/><br/><br/>
+                            {item.name.title + "."}  {item.name.first} {item.name.last} <br/><br/><br/>
                         </li>
                     ))}
                 </ul>
