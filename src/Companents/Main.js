f5c23e82762f4af5c79454c1a2317405186a7318
krasa <sakke9907@gmail.com>
2021-12-08 18:59:28 +0400

    refactoring sort by alphabet

--- src/Companents/Main.js
+++ src/Companents/Main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ModalWindow from './ModalWindow/ModalWindow'
 
+
 export default class Main extends React.Component {
     state = {
         error: null,
@@ -10,7 +11,8 @@ export default class Main extends React.Component {
         setIsOpen: false,
         modalId: 0,
         array: [],
-        value: ""
+        value: "",
+        id: 0
     }
 
 
@@ -41,28 +43,26 @@ export default class Main extends React.Component {
 
     closeModal = () => this.setState({setIsOpen: !this.state.setIsOpen});
 
-    sortByFirstName = (event) => {
-        this.setState({value: event.target.value})
-        this.state.items.map((obj) => {
-            this.state.array.push(obj);
-            if (this.state.value === "sortByAlphabet") {
-                return this.state.array.sort((a, b) => a.name.first.localeCompare(b.name.first));
-            } else {
-                return this.state.array.sort((a, b) => b.name.first.localeCompare(a.name.first));
+    sortByFirstName = (e) => {
+        this.setState({value: e.target.value})
+        this.state.items.sort((a, b) => {
+            if (this.state.value === "Сортировка по алфавиту") {
+                return b.name.first.localeCompare(a.name.first)
+            } else if (this.state.value === "Обратная сортировка") {
+                return a.name.first.localeCompare(b.name.first)
             }
+            return this.state.items;
         })
     }
-        /*this.state.array.push(object.name.first);
-        return this.state.array.sort((a,b) => a.localeCompare(b));*/
 
+    _renderError = () => <p>Error {this.state.error.message}</p>
 
-
-   // _renderError = () => <p>Error {this.state.error.message}</p>
+    _renderLoading = () => <p>Loading...</p>
 
    // _onClose = () => this.setState({ activeId: null })
 
     render() {
-        const {error, isLoaded, array} = this.state;
+        const {error, isLoaded, items, modalId} = this.state;
 
         // return(
         //     <div>
@@ -72,39 +72,40 @@ export default class Main extends React.Component {
         // )
 
         if (error) {
-            return <p>Error {error.message}</p>
+            return this._renderError()
         }
 
         if (!isLoaded) {
-            return <p>Loading...</p>
+            return this._renderLoading()
         }
 
         return (
             <div className="content-container">
                 <select id="selectSort" onChange={this.sortByFirstName}>
-                    <option id="sortByAlphabet" value="Сортировка по алфавиту">Сортировка по алфавиту</option>
+                    <option id="sortByAlphabet" value="Сортировка по алфавиту" >Сортировка по алфавиту</option>
                     <option id="sortByReverseAlphabet" value="Обратная сортировка">Обратная сортировка</option>
                 </select>
-                <ul>
-                    {array.map((item, i) => (
-                        <li key={i}>
-                            {/*SortByName array={this.state.array} object={this.state.items}*/}
-                            <img src={item.picture.medium} onClick={() => { this.openModal(i) }} alt="avatar"/>
-                            <ModalWindow show={ this.state.setIsOpen && this.state.modalId === i } handleClose={ this.closeModal }>
-                                <img src={item.picture.large} alt="avatar"/>
-                                <ul>
-                                    <li>{"street: " + item.location.street}</li>
-                                    <li>{"city: " + item.location.city}</li>
-                                    <li>{"state: " + item.location.state}</li>
-                                    <li>{"email: " + item.email}</li>
-                                    <li>{"phone: " + item.phone}</li>
-                                    <li>{"postcode: " + item.location.postcode}</li>
-                                </ul>
-                            </ModalWindow><br/>
-                            <span>{`${item.name.title}. ${item.name.first} ${item.name.last}`}</span> <br/><br/><br/>
-                        </li>
-                    ))}
-                </ul>
+                <div>
+                    <ul>
+                        {items.map(( item, i) => (
+                            <li key={ i }>
+                                <img src={ item.picture.medium } onClick={() => { this.openModal(i) }} alt="avatar"/>
+                                <ModalWindow show={ this.state.setIsOpen && modalId === i } handleClose={ this.closeModal }>
+                                    <img src={ item.picture.large } alt="avatar"/>
+                                    <ul>
+                                        <li>{ `street: ${ item.location.street }` }</li>
+                                        <li>{ `city: ${ item.location.city }` }</li>
+                                        <li>{ `state: ${ item.location.state }` }</li>
+                                        <li>{ `email: ${ item.email }` }</li>
+                                        <li>{ `phone: ${ item.phone }` }</li>
+                                        <li>{ `postcode: ${ item.location.postcode }` }</li>
+                                    </ul>
+                                </ModalWindow><br/>
+                                <span>{`${ item.name.title }. ${ item.name.first } ${ item.name.last }`}</span> <br/><br/><br/>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         )
     }
